28191adbc04727ea8c04d37ab30b16009235fbe5
unclebabykern <coren.frankel@gmail.com>
2024-12-07 23:38:32 -0700

    add mailto link

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import { Header } from './components/Header'
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <p>More coming soon...</p>
+      <p>Reach me at <a href="mailto:dev@corenfrankel.com">dev@corenfrankel.com</a></p>
+    </>
   )
 }
 
