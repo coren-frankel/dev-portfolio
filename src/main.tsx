c6b054932dae5422cebab6c6b2c30d526a36c40c
unclebabykern <coren.frankel@gmail.com>
2024-12-14 16:41:21 -0700

    feat: migrate main to subcomponent route + routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from "react-router";
 import './index.css'
 import App from './App.tsx'
+import Main from './components/Main.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="home" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
