28ff49fb3e3bac2dbc33d4ea9e758c7f6f47df3d
unclebabykern <coren.frankel@gmail.com>
2025-01-12 21:59:12 -0700

    refactor: extract subcomponent

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,97 +1,88 @@
-import '../styles/Landing.css';
-import { Input, Typography } from 'antd';
-import { PercentageOutlined } from '@ant-design/icons';
-import { useCallback, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router';
-
-const textChunks = [
-  "Hiya! I'm Coren Frankel, but some call me \"Kern\"",
-  "I'm a Software Developer from Denver, CO",
-  "Welcome to my personal website!",
-  "Here's how to navigate:",
-  "Type 'home' to go to the home page",
-  "Type 'about' to learn more about me",
-  "Type 'contact' to reach out",
-  "Press Enter to execute command"
-];
-
-
-const TextChunks = (
-  { chunks, delay = 1 }:
-    { chunks: string[], delay?: number }) => {
-  return (
-    <>
-      {chunks.flatMap((chunk, chunkIdx) => (
-        <span
-          key={`chunk-${delay ? 'pri' : 'com'}-${chunkIdx}`}
-          className='chunk'
-        >
-          {chunk.split('').map((char, idx) => (
-            <span
-              key={`${delay ? 'pri' : 'com'}-${idx * chunkIdx}`}
-              className="inline"
-              style={{ animationDelay: `${delay + (chunkIdx + idx * 0.025)}s` }}
-            >
-              {char}
-            </span>
-          ))}
-        </span>
-      ))
-      }
-    </>
-  )
-}
+import "../styles/Landing.css";
+import { Input, Typography } from "antd";
+import { PercentageOutlined } from "@ant-design/icons";
+import { useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import TextChunk from "./TextChunk";
 
 const Landing = () => {
-  const [command, setCommand] = useState('');
+  const [command, setCommand] = useState("");
   const [chunkState, setChunkState] = useState<string[]>([]);
+  const [wakeUp, setWakeUp] = useState(false);
   const navigate = useNavigate();
 
   // Add visible class for fade-in animation
   useEffect(() => {
-    document.querySelector('.landing-page')?.classList.add('visible');
-    return () => document.querySelector('.landing-page')?.classList.remove('visible');
+    document.querySelector(".landing-page")?.classList.add("visible");
+    return () => document.querySelector(".landing-page")?.classList.remove("visible");
   }, []);
+  
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      // Check if the Control or Command key is pressed and the 'X' key is pressed
+      if ((event.ctrlKey || event.metaKey) && event.key === 'x') {
+        event.preventDefault();  // Prevent the default cut operation
+        setChunkState([...chunkState, "Follow the White Rabbit..."]);
+        setTimeout(() => navigate("/the-matrix-has-you"), 3000);
+      }
+    };
 
-  // TODO: Add more hidden routes and terminal commands
-  // labels: enhancement
-  // iframe of NinjaSweeper https://coren-frankel.github.io/NinjaSweeper/
+    // Attach the event listener when the component is mounted
+    window.addEventListener('keydown', handleKeyDown);
+
+    // Clean up the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [chunkState, navigate]);
+  
+  // Upon 30 seconds of inactivity, load Morpheus' greeting
+  useEffect(() => {
+    const wakeUp = setTimeout(() => {
+      setWakeUp(true);
+      setChunkState(["Wake up, Neo...","The Matrix has you..."]);
+    }, 30000);
+    return () => clearTimeout(wakeUp);
+  }, [command]);
   
   // Home page navigation
   const acceptInputCommand = useCallback((command: string) => {
-    if (command.match(/about/i) || command.match(/contact/i) || command.match(/home/i)) {
+    if (command.match(/about/i) || command.match(/contact/i) || command.match(/home/i) || command.match(/arcade/i)) {
       setChunkState([...chunkState, `% ${command}`]);
       setChunkState([...chunkState, `Redirecting to ${command[0].toUpperCase()}${command.substring(1).toLowerCase()} page...`]);
       setTimeout(() => navigate(`/${command.toLowerCase()}`), 1500);
     } else if (command.match(/white rabbit/i)) {
-      setChunkState([...chunkState, 'Follow the white rabbit.']);
+      setChunkState([...chunkState, "Follow the White Rabbit..."]);
       setTimeout(() => navigate("/the-matrix-has-you"), 3000);
     } else if (command.match(/clear/i)) {
       setChunkState([]);
+    } else if (command.match(/help/i)) {
+      setChunkState([]);
+      setWakeUp(false);
     } else {
       setChunkState([...chunkState, `Command: "${command}" not recognized`]);
     }
-    setCommand('');
+    setCommand("");
   }, [chunkState, navigate]);
 
   return (
-    <div className='landing-page'>
+    <div className="landing-page">
       <div className="terminal lime">
         <pre>
-          <Typography.Paragraph code className='lime' id="code">
-            <TextChunks chunks={textChunks} />
-            <TextChunks chunks={chunkState} delay={0.01} />
+          <Typography.Paragraph code className="lime" id="code">
+            {!wakeUp && <TextChunk />}
+            <TextChunk chunks={chunkState} delay={0.01} />
             <Input
-              prefix={<PercentageOutlined className='lime' />}
+              prefix={<PercentageOutlined className="lime" />}
               styles={{
-                prefix: { border: 'none', backgroundColor: '#111' },
-                affixWrapper: { border: 'none', backgroundColor: '#111', boxShadow: 'none' }
+                prefix: { border: "none", backgroundColor: "#110" },
+                affixWrapper: { border: "none", backgroundColor: "#110", boxShadow: "none" }
               }}
               autoFocus
-              autoComplete='none'
-              aria-autocomplete='none'
+              autoComplete="none"
+              aria-autocomplete="none"
               id="command-line"
-              className='lime'
+              className="lime"
               contentEditable
               value={command}
               onChange={(event) => setCommand(event.target.value)}
---
+++ src/components/TextChunk.tsx
@@ -0,0 +1,41 @@
+
+const textChunks = [
+  "Hiya! I'm Coren, but some call me \"Kern\"",
+  "I'm a Software Developer from Denver, CO",
+  "Welcome to my personal website!",
+  "Here's how to navigate:",
+  "Type 'home' to go to the home page",
+  "Type 'about' to learn more about me",
+  "Type 'contact' to reach out",
+  "Press Enter to execute command"
+];
+
+
+interface TextChunkProps {
+  chunks?: string[],
+  delay?: number
+}
+
+export default function TextChunk({ chunks = textChunks, delay = 1 }: TextChunkProps) {
+  return (
+    <>
+      {chunks.flatMap((chunk, chunkIdx) => (
+        <span
+          key={`chunk-${delay === 1 ? 'pri' : 'com'}-${chunkIdx}`}
+          className='chunk'
+        >
+          {chunk.split('').map((char, idx) => (
+            <span
+              key={`${delay === 1 ? 'pri' : 'com'}-${(idx + 1) * (chunkIdx + 1)}`}
+              className="inline"
+              style={{ animationDelay: `${delay + (chunkIdx + idx * 0.025)}s` }}
+            >
+              {char}
+            </span>
+          ))}
+        </span>
+      ))
+      }
+    </>
+  )
+}
