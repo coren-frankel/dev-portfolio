c62cb7f29729031d2a4ea9951e0249d3fb0fb2b0
unclebabykern <coren.frankel@gmail.com>
2025-08-23 19:28:59 -0600

    feat: enhance About page content and add dividers for better readability

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,4 +1,4 @@
-import { Card, Grid } from "antd";
+import { Card, Divider, Grid } from "antd";
 import { Layout } from "../components/Layout";
 import { Link } from "react-router";
 
@@ -39,16 +39,20 @@ const About = () => {
         <div
           style={{ fontSize: isMobile() ? "14px" : "16px", lineHeight: "1.6" }}
         >
-          I&apos;m called Coren. Full Stack Software Developer based in Denver,
+          I&apos;m called Coren. I am a Software Developer based in Denver,
           Colorado. I acquired dual bachelors degrees in Philosophy & Political
-          Science in 2013. For the following 8 years I ascended rank in various
-          roles in the hospitality industy, taking pride in my ability to lead
-          through action and deftly solve multi-tenanted problems. I have always
-          been a tinkerer, and have always had a passion for learning new
-          things, so it was only a matter of time before I found my way to
-          programming.
-          <br />
-          <br />
+          Science in 2013 from Northern Arizona University. For the following 8
+          years I ascended rank in various roles in the hospitality industy,
+          taking pride in my ability to lead through action and deftly solve
+          multi-tenanted problems. I have always been a tinkerer, and have
+          always had a passion for learning new things, so it was only a matter
+          of time before I found my way to programming.
+          <Divider
+            style={{ borderColor: "lavender", color: "lavender" }}
+            orientation="right"
+          >
+            Transition to Code
+          </Divider>
           In 2022, I stepped away from my role as General Manager of a popular
           fast-casual bar & restaurant chain to begin a new chapter, confident
           that my education and work ethic would serve me anywhere. Within a
@@ -57,20 +61,29 @@ const About = () => {
           <Link to="https://www.theodinproject.com/">The Odin Project</Link>
           &apos;s Web Development curriculumn and quickly committed to making a
           career transition into software engineering and web development.
-          <br />
-          <br />
+          <Divider
+            style={{ borderColor: "lavender", color: "lavender" }}
+            orientation="center"
+          >
+            Programming Background
+          </Divider>
           After excelling in two <em>bootcamp</em>-style immersive programs, and
           moonlighting as a Python & JavaScript instructor for K-12 students in
           between, I was able to land my first role as a Full Stack Developer in
           early 2023. Since then I have had the pleasure of leading development
           on a series of projects and codebases using Node & React with
-          TypeScript, and have proven effective with multiple languages and
-          technology stacks. In my short time as a developer I have leveraged my
+          TypeScript, and have proven effective with multiple languages and tech
+          stacks, not limited to maintaining a proprietary Python eCommerce
+          integration. In my short time as a developer I have leveraged my
           experience to embed myself in various communities, providing multiple
           opportunities to mentor and support other developers and to contribute
           meaningfully to Open Source Software projects.
-          <br />
-          <br />
+          <Divider
+            style={{ borderColor: "lavender", color: "lavender" }}
+            orientation="left"
+          >
+            Personal Interests
+          </Divider>
           I enjoy horror novels and philosophy texts for pleasure. I enjoy
           hiking and pontificating with my partner, who is currently a first
           year law student. We are adoptive parents to a lovably
@@ -79,7 +92,6 @@ const About = () => {
           a guitar or ukelele from time to time. I usually have a single-player
           game on rotation on my PS5, and it&apos;s rarely the newest or hottest
           thing.
-          <br />
         </div>
       </Card>
     </Layout>
